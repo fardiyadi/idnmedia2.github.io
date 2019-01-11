e5951a6b8da7b20cb6ebf0e441657e52d7f15f3c
fardiyadi <faizal.icoen@gmail.com>
2019-01-11 14:04:56 +0700

    add more files

---
+++ idn/desktop/assets/js/popup.js
@@ -0,0 +1,7 @@
+function myFunctionClose() {
+    document.getElementById("popup_desktop").style.display = "none";
+}
+    
+setTimeout(function(){
+  document.getElementById("popup_desktop").style.display = "none"; 
+ }, 30000);
