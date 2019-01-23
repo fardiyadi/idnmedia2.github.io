57281ea5247b2ef15e721b990231eed49880a922
fardiyadi <faizal.icoen@gmail.com>
2019-01-23 12:45:05 +0700

    sales product

---
+++ adsproduct/page/sidebar.php
@@ -0,0 +1,293 @@
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<style>
+    .fa {
+        margin-right: 10px;
+        display: inline;
+    }
+</style>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top d-sm-block d-md-none in">
+        <a class="navbar-brand" href="#menu-toggle" id="menu-toggle">Browse Our Ads</a>
+    </nav>
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav accordion">
+                
+                <!-- logo idn media -->
+                
+                <li class="sidebar-brand">
+                    <a href="#"> 
+                        <img src="../assets/images/idn-media-black.png" class="img-fluid">
+                    </a>
+                </li>
+              
+                <!-- idntimes -->              
+                
+                <li>
+                    <a class="toggle" href="javascript:void(0);"><i class="fa fa-angle-down "></i>IDN Times</a>
+                    <ul class="inner nakma">
+                        
+                        <!--  idntimes desktop -->
+                    
+                        <li>
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Desktop</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="../idntimes/br-1.php">Big Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/br-2.php">Big Rectangle 2</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/inart1.php">In Article 1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/inart2.php">In Article 2</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/inart3.php">In Article 3</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/infeed1.php">Infeed 1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/infeed2.php">Infeed 2</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/infeed3.php">Infeed 3</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/leaderboard-1.php">Leaderboard V1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/leaderboard-2.php">Leaderboard V2</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/leaderboard-3.php">Leaderboard V3</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/mr-1.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/mr-2.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/popup.php">Popup</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/skinad.php">Skinad</a>
+                                </li>
+                                <li>
+                                    <a href="../idntimes/sticky.php">Sticky Bottom</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        
+                        <!--  idntimes mobile -->
+                        
+                        <li> 
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Mobile</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="../m.idntimes/inart1-1.php">In Article 1</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/inart2-1.php">In Article 2</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/inart3-1.php">In Article 3</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/infeed1-1.php">Infeed 1</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/infeed2-1.php">Infeed 2</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/infeed3-1.php">Infeed 3</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/leaderboard-1.php">Leaderboard V1</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/leaderboard-2.php">Leaderboard V2</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/parallax1.php">Parallax 1</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/parallax2.php">Parallax 2</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/parallax3.php">Parallax 3</a>
+                                </li>
+                                <li>
+                                    <a href="../m.idntimes/sticky.php">Sticky Bottom</a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </li>          
+                
+                <!-- popbela -->
+                
+                <li>
+                    <a class="toggle" href="javascript:void(0);"><i class="fa fa-angle-down "></i>Popbela</a>
+                    <ul class="inner nakma">
+                        
+                        <!-- popbela desktop -->
+                        
+                        <li>
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Desktop</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="../popbela/br1.php">Big Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/br2.php">Big Rectangle 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/inart1.php">In Article 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/inart2.php">In Article 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/inart3.php">In Article 3</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/infeed1.php">Infeed 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/infeed2.php">Infeed 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/infeed3.php">Infeed 3</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/leaderboard-1.php">Leaderboard V1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/leaderboard-2.php">Leaderboard V2</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/leaderboard-3.php">Leaderboard V3</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/mr1.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/mr2.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/popup.php">Popup</a>
+                                </li>
+                                <li>
+                                    <a href="../popbela/sticky.php">Sticky Bottom</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        
+                        <!-- popbela mobile -->
+                        
+                        <li>
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Mobile</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="#">n/a</a>
+                                </li>
+                                <li>
+                                    <a href="#">n/a</a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </li>
+                
+                <!-- popmama -->
+                
+                <li>
+                    <a class="toggle" href="javascript:void(0);"><i class="fa fa-angle-down "></i>Popmama</a>
+                    <ul class="inner nakma">
+                        
+                        <!-- popmama desktop -->
+                        
+                        <li>
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Desktop</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="../popmama/br1.php">Big Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/br2.php">Big Rectangle 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/inart1.php">In Article 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/inart2.php">In Article 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/inart3.php">In Article 3</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/infeed1.php">Infeed 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/infeed2.php">Infeed 2</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/infeed3.php">Infeed 3</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/leaderboard-1.php">Leaderboard V1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/leaderboard-2.php">Leaderboard V2</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/leaderboard-3.php">Leaderboard V3</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/mr1.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/mr2.php">Medium Rectangle 1</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/popup.php">Popup</a>
+                                </li>
+                                <li>
+                                    <a href="../popmama/sticky.php">Sticky Bottom</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        
+                        <!-- popmama mobile -->
+                        
+                        <li>
+                            <a href="#" class="toggle"><i class="fa fa-angle-down "></i>Mobile</a>
+                            <ul class="inner nakwa">
+                                <li>
+                                    <a href="#">n/a</a>
+                                </li>
+                                <li>
+                                    <a href="#">n/a</a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </li>
+            </ul>
+        </div>
+</div>
