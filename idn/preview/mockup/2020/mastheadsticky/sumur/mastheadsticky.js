6708e9bb1f27c62e1b0ff75d93b7a57656bad410
Faizal Ardiyadi <faizal.icoen@gmail.com>
2020-10-14 18:05:19 +0700

    sgdrg

--- idn/preview/mockup/2020/mastheadsticky/sumur/mastheadsticky.js
+++ idn/preview/mockup/2020/mastheadsticky/sumur/mastheadsticky.js
@@ -2,7 +2,7 @@ if(top!=self){
     window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 320px; position: absolute; bottom: 0; left:0; right:0; margin:auto;';
 }
 
-$('#mastheadsticky').animate({'height':'45px'},"slow");
+$('#mastheadsticky').animate({'height':'65px'},"slow");
 $('.ms_big').animate({'height':'0px'},"slow");
 $('.ms_small').fadeIn('slow');
 
@@ -42,84 +42,6 @@ setTimeout(function(){
 }, 5000);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//var expandOverlay = function(){
-//    if(top!=self){
-//         window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 100vh; width: 100vw; position: fixed; left:0; right: 0; margin: auto; top: 50%; transform: translateY(-50%);'; 
-//    };
-//    $("#bs02").css({"display" : "block"});
-//}
-//
-//var closeExpandOverlay = function(){
-//    if(top!=self){
-//         window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 50px; width: 320px; position: fixed; left:0; right: 0; margin: auto; bottom: 0;'; 
-//    };
-//    $("#bs02").css({"display" : "none"});
-//}
-//
-//
-//
-//
-//
-//$('#bs01').click(function() {
-//    if(top!=self){
-//        window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 100vh; width: 100vw; position: fixed; left:0; right: 0; margin: auto; top: 50%; transform: translateY(-50%);'; 
-//    }
-//     $("#bs02").css({"display" : "block"});
-//});
-//$('.closeBtn02').click(function() {
-//    if(top!=self){
-//        window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 50px; width: 320px; position: fixed; left:0; right: 0; margin: auto; bottom: 0;'; 
-//    }
-//    $("#bs02").css({"display" : "none"});
-//});
-
-
-
-
-
-
-
-
-
-
-//$(document).ready(function(){
-//    setTimeout(function(){ 
-//        expandOverlay();
-//    }, 1000);  
-//     setTimeout(function(){ 
-//        closeExpandOverlay();
-//    }, 3000); 
-//});
-//
-//
 
 
 
